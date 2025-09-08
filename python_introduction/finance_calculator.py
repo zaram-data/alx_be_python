8f576a8b39aa398070e1d862736e6fe39b1dd915
Daniel Isaac-orji <loidbrix@gmail.com>
2025-09-08 17:35:50 +0100

    Fix variable names for finance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,13 +1,13 @@
 # Personal Finance Calculator
 
 # Ask user for monthly income
-income = float(input("Enter your monthly income: "))
+monthly_income = float(input("Enter your monthly income: "))
 
 # Ask user for monthly expenses
-expenses = float(input("Enter your total monthly expenses: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
 # Calculate monthly savings
-monthly_savings = income - expenses
+monthly_savings = monthly_income - monthly_expenses
 
 # Project annual savings with 5% interest
 annual_savings = monthly_savings * 12
@@ -16,3 +16,4 @@ projected_savings = annual_savings + (annual_savings * 0.05)
 # Display the results
 print("Your monthly savings are $", monthly_savings)
 print("Projected savings after one year, with interest, is: $", projected_savings)
+
